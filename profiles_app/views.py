aaadc88f2128b64a4b9f5ed077564c358a468a2c
sbdeveloper <dev.sbdeveloper@gmail.com>
2025-05-05 08:27:12 +0100

    Added HelloApiView to urls

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -3,9 +3,7 @@ from rest_framework.response import Response
 
 
 class HelloApiView(APIView):
-    """
-    Test API View
-    """
+    """Test API View"""
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
