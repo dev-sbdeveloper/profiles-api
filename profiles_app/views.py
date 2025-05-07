037796c77a03ea1f42590b2a9b09cd1c1fa57932
sbdeveloper <dev.sbdeveloper@gmail.com>
2025-05-07 10:44:47 +0100

    Added profiles viewset

--- profiles_app/views.py
+++ profiles_app/views.py
@@ -2,7 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+
 from profiles_app import serializers
+from profiles_app import models
+from profiles_app import permissions
 
 
 class HelloApiView(APIView):
@@ -87,3 +92,12 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
