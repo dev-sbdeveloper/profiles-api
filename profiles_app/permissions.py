af31a311759c8ca737159dd3a51f1f23b141d1ac
sbdeveloper <dev.sbdeveloper@gmail.com>
2025-05-08 11:00:22 +0100

    Added user profile feed API

--- profiles_app/permissions.py
+++ profiles_app/permissions.py
@@ -9,3 +9,13 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id == request.user.id
+
+
+class UpdateOwnFeed(permissions.BasePermission):
+    """Allow users to update their own feed status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check that user is trying to update their own feed status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile_author.id == request.user.id
